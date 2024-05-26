31ef1ad5bad854d666d8a85e508f65b11f6bc161
Harsh Vardhan <harsh.1vardhan123@gmail.com>
2024-05-27 01:06:36 +0530

    update

--- constants/config.js
+++ constants/config.js
@@ -1,15 +1,14 @@
 const corsOptions = {
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:4173",
-      "https://chat-app-frontend-ivory.vercel.app",
-      process.env.CLIENT_URL,
-    ],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  };
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:4173",
+    process.env.CLIENT_URL,
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
   
-  const CHATAPP_TOKEN = "chatApp-token";
+const CHATAPP_TOKEN = "chatApp-token";
   
-  export { corsOptions, CHATAPP_TOKEN }; 
+export { corsOptions, CHATAPP_TOKEN }; 
